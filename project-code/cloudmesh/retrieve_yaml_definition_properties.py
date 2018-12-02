5536c261760d91c54dc00f69c6d6cca4acb0d6a3
Richa Rastogi <rirastog@iu.edu>
2018-12-01 17:59:07 -0800

    Adding DB insertion

--- project-code/cloudmesh/retrieve_yaml_definition_properties.py
+++ project-code/cloudmesh/retrieve_yaml_definition_properties.py
@@ -1,18 +1,35 @@
 import yaml
+import os
 from pprint import pprint
 
-with open('file.yml', 'r') as f:
-    # use safe_load instead load
-    file = yaml.safe_load(f)
+prefix_path = "../"
+file_array = [f for f in os.listdir(prefix_path) if f.endswith('file.yml')]
+file_array.sort()  # file is sorted list
 
-with open('user.yml', 'r') as f:
-    # use safe_load instead load
-    user = yaml.safe_load(f)
+file_array = [os.path.join(prefix_path, name) for name in file_array]
+
+for filename in file_array:
+    with open(filename, 'r') as f:
+        # use safe_load instead load
+        file = yaml.safe_load(f)
+
+
+user_array = [f for f in os.listdir(prefix_path) if f.endswith('user.yml')]
+user_array.sort()
+
+user_array = [os.path.join(prefix_path, name) for name in user_array]
+
+for filename in user_array:
+    with open(filename, 'r') as f:
+        # use safe_load instead load
+        user = yaml.safe_load(f)
+
 
 propertyname = []
 
+
 def generate(type):
-    if type == "File":
+    if type == 'File':
         for fileproperty in file['definitions']['File']['properties']:
             propertyname.append(fileproperty)
 
@@ -24,5 +41,7 @@ def generate(type):
         return propertyname
 
 
+
 #property = generate("User")
+#print(property)
 
