40868a36d433fd04912b61166252f279e0a245d5
Gregor von Laszewski <laszewski@gmail.com>
2018-12-05 16:06:38 -0500

    add config

--- project-code/test/test_config.py
+++ project-code/test/test_config.py
@@ -2,13 +2,20 @@ from cloudmesh_data.data.Config import Config
 from cloudmesh_data.data.util.debug import myself, HEADING
 from pprint import pprint
 
+# nosetests -v --nocapture tests/test_config.py
+
+
 def test_config():
     HEADING(myself())
     config = Config()
 
     print(config)
 
+    print(config.dict())
+
     #pprint(config.credentials('local'))
 
     assert config is not None
+
     #assert 'cloud' in config.cloud
+
