7eee2e3ff6f24c111285da10dd497995b351e8bb
Richa Rastogi <rirastog@iu.edu>
2018-12-04 18:47:02 -0800

    Changing google.py to include DataProvider format

--- project-code/cloudmesh_data/data/provider/google.py
+++ project-code/cloudmesh_data/data/provider/google.py
@@ -1,14 +1,11 @@
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 from httplib2 import Http
 from googleapiclient import discovery
-import os
-from prettytable import PrettyTable
 from google.cloud import storage
 import os
-import yaml
 import logging
+from cloudmesh_data.data.provider.DataProviderABC import DataProviderABC
 from cloudmesh_data.data.Config import Config
-
 
 # BUG: from pypi
 
@@ -18,8 +15,19 @@ from cloudmesh_data.data.Config import Config
 #
 # this needs review due to bucket name also being returned.
 #
+from cloudmesh_data.data.provider.local import LocalProvider
 
-class Google(object):
+
+class Google(DataProviderABC):
+
+    def __init__(self, cloud=None):
+        if cloud is None:
+            self.cloud = "google"
+        else:
+            self.cloud = cloud
+
+        localprovider = LocalProvider()
+        self.dir = LocalProvider.create(localprovider, str(os.getcwd()), 'test')
 
     def authenticate(self):
         logging.basicConfig(filename='debug.log', level=logging.DEBUG)
@@ -30,15 +38,12 @@ class Google(object):
             config.credentials('google_cloud')['GOOGLE_CLOUD_CREDENTIALS_JSON']
 
         #
-        # Why setting the os environment we have thisin config
+        # Why setting the os environment we have this in config
         #
         self.storage_client = storage.Client()
-        # The name for the bucket
-        self.bucket_name = config['cloud']['google_cloud']['bucket_name']
-        # print(bucket_name)
-        return self.storage_client, self.bucket_name
+        return self.storage_client
 
-    def upload_blob(self, bucketname, filename):
+    def upload(self, bucketname, filename):
         """Uploads a file to the bucket."""
         bucket = self.storage_client.get_bucket(bucketname)
         blob = bucket.blob(filename)
@@ -47,23 +52,7 @@ class Google(object):
             self.config['local_directory'] + filename,
             filename))
 
-    # upload_blob('MapReduce.docx')
-
-    def listFiles(self):
-        """
-
-        :return:
-        """
-        # aws_files = s3List.list_objects()
-        google_files = self.list_object()
-        # combined_files = aws_files.copy()
-        # combined_files.extend(google_files)
-        # print(aws_files)
-        # print(google_files)
-        for file in google_files:
-            print(file)
-
-    def list_object(self, bucketname):
+    def list(self, bucketname):
         bucket = self.storage_client.get_bucket(bucketname)
         blobs = bucket.list_blobs()
         keys = []
@@ -72,16 +61,16 @@ class Google(object):
 
         return keys
 
-    def create_service(self, bucket_name):
+    def create(self, bucket_name):
         bucket = self.storage_client.create_bucket(bucket_name)
         print('Bucket {} created.'.format(bucket.name))
         scopes = ['https://www.googleapis.com/auth/devstorage.read_write']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            self.config['cloud']['google_cloud']['credentials']['GOOGLE_CLOUD_CREDENTIALS_JSON'], scopes)
-        http_auth = credentials.authorize(Http())
-        return discovery.build('storage', 'v1', http=http_auth)
+        #credentials = ServiceAccountCredentials.from_json_keyfile_name(
+         #   self.config['cloud']['google_cloud']['credentials']['GOOGLE_CLOUD_CREDENTIALS_JSON'], scopes)
+        #http_auth = credentials.authorize(Http())
+        #return discovery.build('storage', 'v1', http=http_auth)
 
-    def delete_blob(self, bucket_name, blob_name):
+    def delete(self, bucket_name, blob_name):
         """Deletes a blob from the bucket."""
         bucket = self.storage_client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name)
@@ -89,20 +78,21 @@ class Google(object):
         print('Blob {} deleted.'.format(blob_name))
 
     # Function to download a file from google cloud
-    def download_blob(self, bucketname, filename):
+    def download(self, bucketname, filename):
         """Downloads a blob from the bucket."""
         bucket = self.storage_client.get_bucket(bucketname)
         blob = bucket.blob(filename)
-        file_path = self.config['local_directory'] + filename
+        file_path = self.dir + filename
         blob.download_to_filename(file_path)
         # print('Blob {} downloaded to {}.'.format(filename, self.config['local_directory']+filename))
         return file_path
 
     # Function to download a full dir from google cloud
-    def download_dir(self, bucketname, prefix):
-        os.mkdir(self.config['local_directory'] + prefix)
-        dl_dir = self.config['local_directory'] + prefix
-        print(dl_dir)
+    def download_dir(self, bucketname, prefix, filename):
+        if prefix != '' and filename == '':
+            os.mkdir(self.config['local_directory'] + prefix)
+            dl_dir = self.config['local_directory'] + prefix
+
         bucket = self.storage_client.get_bucket(bucketname)
         blobs = bucket.list_blobs(prefix=prefix)  # Get list of files
         for blob in blobs:
@@ -111,8 +101,11 @@ class Google(object):
             blob.download_to_filename(dl_dir + filename)  # Download
         return dl_dir
 
-# path = download_dir('richa-cloud-516', 'MapReduce.docx')
+    def copy(self):
+        pass
 
-# create_service('test_richa')
-# delete_blob('richa-cloud-516','MapReduce.docx')
-# print(list_object('richa-cloud-516'))
+    def move(self):
+        pass
+
+
+
